0954431dda594b8a8e2487718c84b234688d9b04
Stefan Mac <s.vucan@levi9.com>
2023-11-13 10:20:32 +0100

    mock login and registration screens

--- app/src/main/java/com/example/cocktailsdbapp/MainActivity.kt
+++ app/src/main/java/com/example/cocktailsdbapp/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.cocktailsdbapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        navController = findNavController(R.id.nav_host_fragment)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationMenu)
+
+        bottomNav.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.loginFragment || destination.id == R.id.registrationFragment) {
+                bottomNav.visibility = View.GONE
+            } else {
+                bottomNav.visibility = View.VISIBLE
+            }
+        }
+
     }
 }
