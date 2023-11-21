7ea2345e4a71a3a54f3cbd5732c43191d8ba8738
Stefan Mac <s.vucan@levi9.com>
2023-11-21 14:24:33 +0100

    -Network package update

--- app/src/main/java/com/example/cocktailsdbapp/network/CocktailResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.example.cocktailsdbapp.network
-
-import com.google.gson.annotations.SerializedName
-
-data class CocktailResponse(
-    @SerializedName("drinks")
-    val drinks: List<Cocktail>
-)
